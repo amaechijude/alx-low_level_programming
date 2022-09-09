3255ebb5a8b76422d76a96801e5e04c488c47c67
amaechijude <ugwuamaechi5@gmai.com>
2022-09-08 20:35:39 -0700

    Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main indicates the starting point of the code
+ * return by default is 0 indicating success if there is no error
+ * */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
