cd052989565ca8c37469416cb1cd4bd28b779676
amaechijude <ugwuamaechi5@gmai.com>
2022-09-13 12:10:23 -0700

    1st nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - Prints '_printchar' followed by a new line
+ *
+ * Return 0 if no error is present
+ */
+int main(void)
+{
+	printf(_putchar\n);
+
+	return (0);
+}
