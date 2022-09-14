59dc56a57252f364e8dd4278b2d272e8452085b5
amaechijude <ugwuamaechi5@gmai.com>
2022-09-14 04:36:20 -0700

    MainH

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print _putchar with a new line
+ *
+ * Return = 0: for success
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	
+	return (0);
+}
