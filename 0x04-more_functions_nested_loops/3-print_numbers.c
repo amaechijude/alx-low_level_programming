52e49e757a18ad45bf07a2636caab986ed87dccc
amaechijude <ugwuamaechi5@gmail.com>
2022-09-15 20:31:16 -0700

    Count more

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 
+ * Returns 
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar('c'):
+	}
+	_putchar('\n');
+}
