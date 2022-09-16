44d2b8ea58de64d08c344885641e19c7958d235f
amaechijude <ugwuamaechi5@gmail.com>
2022-09-15 20:58:45 -0700

    Draw lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main -draws a shortest distance between two points
+ * @n is the number of times
+ * Returns 
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
