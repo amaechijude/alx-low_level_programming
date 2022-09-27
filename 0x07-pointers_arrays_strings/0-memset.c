ee019b91adeccc473840e90feba05f5484344d36
amaechijude <ugwuamaechi5@gmail.com>
2022-09-27 13:09:58 -0700

    Memcopy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,24 @@
-#include "main.h"
+#include "main.h"
 
 /**
- * _memset - Entry point
- * Description - A function that fills memory with a constant byte
- * @s: the function accepts an input saved into s
- * @b: the function accepts an input saved into b
- * @n: the function accepts an input saved into n
- * Return: a char as success
+ * _memset - Entry point
+ * Description - A function that fills memory with a constant byte
+ * @s: the function accepts an input saved into s
+ * @b: the function accepts an input saved into b
+ * @n: the function accepts an input saved into n
+ * @n: the function accepts an input saved into n
+ * Return: a char as success
  */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
-	
-	while (i < n)
+	unsigned int i = 0;
+
+	while (i < n)
 	{
-		s[i] = b;
+		s[i] = b;
 		i++;
 	}
-	return (s);
+
+	return (s);
 }
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,24 @@
-#include "main.h"
-#include <string.h>
+#include "main.h"
+#include <string.h>
 
 /**
- * _strchr - Entry point
- * Description - A function that locates a character in a string
- * @s: the function accepts an input saved into s
- * @c: the function accepts an input saved into c
- * Return: Always 0
+ * _strchr - Entry point
+ * Description - A function that locates a character in a string
+ * @s: the function accepts an input saved into s
+ * @c: the function accepts an input saved into c
+ * Return: Always 0
  */
 
-char *_strchr(char *s, char c)
+char *_strchr(char *s, char c)
 {
-	int i = 0;
-	
-	while (s[i] >= '\0')
+	int i = 0;
+
+	while (s[i] >= '\0')
+
 	{
-		if (s[i] == c)
-			return (&s[i]);
+		if (s[i] == c)
+			return (&s[i]);
 		i++;
 	}
-	return (NULL);
+	return (NULL);
 }
